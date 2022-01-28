50e178694d7cad8cd7aaa5bbb97e628e118155c1
Austin Warner <awarner0949@gmail.com>
2022-01-28 16:40:00 -0500

    Completed main game logic. Player vs computer

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 function computerPlay() {
     const choices = ['rock', 'paper', 'scissors'];
     computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    console.log(`Computer choice: ${computerChoice}`);
     return computerChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    message = '';
+    //Scenarios where player plays rock
+    if(playerSelection === 'rock') {
+        if(computerSelection === 'paper') {
+            message = 'You lose! Paper beats rock!';
+        } else if(computerSelection === 'scissors') {
+            message = 'You win! Rock beats scissors!';
+        } else {
+            message = 'It\'s a tie';
+        }
+    } else if(playerSelection === 'paper') {
+        if(computerSelection === 'rock') {
+            message = 'You win! Paper beats rock!';
+        } else if(computerSelection === ' scissors') {
+            message = 'You lose! Scissors beat paper';
+        } else {
+            message = 'It\'s a tie';
+        }
+    } else if(playerSelection === 'scissors') {
+        if(computerSelection === 'rock') {
+            message = 'You lose! Rock beats scissors!';
+        } else if(computerSelection === ' paper') {
+            message = 'You win! Scissors beat paper';
+        } else {
+            message = 'It\'s a tie';
+        }
+    } else {
+        message = 'Please enter either rock, paper, or scissors';
+    }
+
+    return message;
 }
